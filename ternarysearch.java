1257dc5a981bbbf84aba90b3d065a96a5ba9bc9b
Will Cromar <wcromar@knights.ucf.edu>
2017-03-29 12:03:02 -0400

    Improved ternary search code slightly.

--- ternarysearch.java
+++ ternarysearch.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
 public class ternarysearch {
+	static double EPS = .1;
+
 	static private double tern(double lo, double hi) {
-		while (hi - lo > .1) {
+		while (hi - lo > EPS) {
 			double ft = lo + (hi - lo) / 3;
 			double st = hi - (hi - lo) / 3;
 			
-	        if (f(ft) > f(st))
+	        if (f(ft) >= f(st))
 	            hi = st;
 	        else
 	            lo = ft;
